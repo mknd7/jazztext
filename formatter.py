0eebdeb50984e584da8a5e009fd82a35c4b776b3
Mukund Vijayaraghavan <mkndviji@gmail.com>
2017-04-28 12:14:33 +0530

    Remove capitalization

--- formatter.py
+++ formatter.py
@@ -23,128 +23,104 @@ def mformat_default(input, output):
 # perform a simple format
 def simple_format(line):
     res = ''                                            # formatted string for each line
-    first_letter = False                                # first letter occurence for every sentence in a line
-    punc_marks_capitalize = ['.','!','?']
-    punc_marks_normal = [',',':',';','%']
+    punc_marks_all = ['.','!','?',',',':',';','%']
     newlines = ['\n','\r']
-
-    # list of all punctuation marks
-    punc_marks_all = punc_marks_capitalize + punc_marks_normal
 
     # loop over every character in line
     i = 0
-    while i < len(line): 
-
-        # first letter not occurred yet
-        if line[i] != ' ' and not first_letter:
-            first_letter = True
-            if line[i] in newlines:
-                res += line[i]
-            if line[i].isalnum():
-                res += line[i].upper()
-            else:
-                if line[i] in ['(',')','[',']','"',"'"]:
-                    res += line[i]
-                elif line[i] in punc_marks_all and line[i + 1] in newlines:
-                    res += line[i]
-                # to eliminate duplicates
-                if len(res) >= 2:
-                    if res[-2] == line[i]:
-                        first_letter = False
-
-        # first letter has already occured
-        elif first_letter:
-            # any char in ['.','!','?']
-            if line[i] in punc_marks_capitalize:
-                first_letter = False
-            # for 'etc.'
-            if line[i] == '.' and line[i-3 : i] == 'etc':
-                first_letter = True
-                res += '.'
-                i += 1
-                continue
+    while i < len(line):
+        if line[i] in newlines:
+            res += line[i]
+        # for handling a single space
+        elif line[i] == ' ':
+            if i + 1 < len(line):
+                if line[i + 1] not in punc_marks_all + [' ']:
+                    if len(res) >= 1:
+                        if res[-1] != ' ':
+                            res += ' '
+        elif line[i] in punc_marks_all:
             # for ellipsis (...)
             if line[i : i + 3] == '...':
-                first_letter = True
                 if res[-3:] != '...':
                     res += '...'
                 i += 3
                 continue
-            # if any punctuation mark is encountered
-            if line[i] in punc_marks_all:
-                # to eliminate duplicates
-                if len(res) >= 2:
-                    if res[-2] == line[i] and res[-1] == ' ':
+            # to eliminate duplicate punctuation marks
+            if len(res) >= 2:
+                if res[-2] == line[i] and res[-1] == ' ':
+                    i += 1
+                    continue
+            res += line[i]
+            # ensuring no comma spaces after commas in-between digits (e.g. 7,000)
+            if line[i] == ',':
+                if i + 1 < len(line):
+                    if line[i + 1].isdigit():
                         i += 1
                         continue
-                res += line[i]
-                # ignore comma spaces for commas between digits (eg. 7,000)
-                if line[i] == ',':
-                    if i + 1 < len(line):
-                        if line[i + 1].isdigit():
-                            i += 1
-                            continue
-                # do not add spaces between version number digits (eg. v. 1.0)
-                elif line[i] == '.':
-                    if i + 1 < len(line):
-                        if line[i - 1].isdigit() and line[i + 1].isdigit():
-                            first_letter = True
-                            i += 1
-                            continue
-                        elif line[i - 1].isdigit() and line[i + 1] == 'x':
-                            first_letter = True
-                            i += 1
-                            continue
+            # ensuring no spaces after fullstops in version numbers (e.g. v1.0)
+            elif line[i] == '.':
                 if i + 1 < len(line):
-                    if line[i + 1] not in newlines + ['.',')',']','"',"'"] and res[-1] != ' ':
+                    if line[i - 1].isdigit() and line[i + 1].isdigit():
+                        i += 1
+                        continue
+                    elif line[i - 1].isdigit() and line[i + 1] == 'x':
+                        i += 1
+                        continue
+            # single space after punctuation mark
+            if i + 1 < len(line):
+                if line[i + 1] not in newlines + [')',']','"',"'"] and line[i + 1 : i + 4] != 'NET':
+                    if res[-1] != ' ':
                         res += ' '
-
-            # for any other character or phrases
-            elif line[i] != ' ':
-                if line[i : i + 3] == "i'm":
-                    res += line[i].upper()
-                elif line[i : i + 2] == 'i ':
-                    res += line[i].upper()
-                elif line[i : i + 4] == 'i.e.' or line[i : i + 4] == 'i.e ':
-                    res += 'i.e. '
-                    i += 4
-                    continue
-                elif line[i : i + 3] == 'ie.':
-                    res += 'i.e. '
-                    i += 3
-                    continue
-                elif line[i : i + 4] == 'e.g.' or line[i : i + 4] == 'e.g ':
-                    res += 'e.g. '
-                    i += 4
-                    continue
-                elif line[i : i + 3] == 'eg.':
-                    res += 'e.g. '
-                    i += 3
-                    continue
-                elif line[i : i + 2] == 'v.':
-                    res += 'v.'
-                    i += 2
-                    continue
-                elif line[i : i + 2] == 'i.':
-                    res += line[i].upper()
-                # for handling parens
-                elif line[i : i + 2] == '()':
-                    res += '()'
-                    i += 2
-                    continue
-                elif line[i] == '(':
-                    if res[-1] == ' ':
-                        res += '('
-                    else:
-                        res += ' ('
+        # for any other character or phrases
+        else:
+            if line[i : i + 3] == "i'm":
+                res += line[i].upper()
+            elif line[i : i + 2] == 'i ':
+                res += line[i].upper()
+            elif line[i : i + 2] == 'i.':
+                res += line[i].upper()
+            elif line[i : i + 2] == 'i,':
+                res += line[i].upper()
+            elif line[i : i + 4] == 'i.e.' or line[i : i + 4] == 'i.e ':
+                res += 'i.e. '
+                i += 4
+                continue
+            elif line[i : i + 3] == 'ie.':
+                res += 'i.e.'
+                i += 3
+                continue
+            elif line[i : i + 4] == 'e.g.' or line[i : i + 4] == 'e.g ':
+                res += 'e.g. '
+                i += 4
+                continue
+            elif line[i : i + 3] == 'eg.':
+                res += 'e.g. '
+                i += 3
+                continue
+            elif line[i : i + 4] == 'etc.':
+                res += 'etc.'
+                i += 4
+                continue
+            elif line[i : i + 3] == 'etc':
+                res += 'etc.'
+                i += 3
+                continue
+            elif line[i : i + 2] == 'v.':
+                res += 'v.'
+                i += 2
+                continue
+            # for handling parens
+            elif line[i : i + 2] == '()':
+                res += '()'
+                i += 2
+                continue
+            elif line[i] == '(':
+                if res[-1] == ' ':
+                    res += '('
                 else:
-                    res += line[i]
-
-            # check for single space character
-            elif line[i] == ' ':
-                if i + 1 < len(line):
-                    if line[i + 1] not in (punc_marks_all + [' ']) and res[-1] != ' ':
-                        res += ' '
+                    res += ' ('
+            else:
+                res += line[i]
         i += 1
     return res
 
